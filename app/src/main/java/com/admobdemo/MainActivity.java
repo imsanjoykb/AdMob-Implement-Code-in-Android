47b814e368f3ce27a59789e1d36c9d2d53214b2a
Sanjoy Biswas <sanjoy.eee32@gmail.com>
2019-06-21 11:56:06 +0600

    first commit

---
+++ app/src/main/java/com/admobdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.admobdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
